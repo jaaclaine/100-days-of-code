057d8d398c639ca26605430a95f6e20ea7f599f1
Jaaclaine <jaquelinesodre@live.com>
2017-06-14 21:55:49 -0300

    Don't stop believing :dancer:

--- Text Page/script.js
+++ Text Page/script.js
@@ -1,1 +1,13 @@
-
+// Show / Hide top bar when scroll
+document.addEventListener("scroll", topBar);
+var scrollBar = 0;
+function topBar(){
+  var bar = document.getElementById("top-bar");
+  var scrollTop = document.getElementsByTagName("body")[0].scrollTop;
+  if(scrollBar > scrollTop || scrollBar < 200){
+    bar.classList.remove("active");
+  } else{
+    bar.classList.add("active");
+  }
+  scrollBar = scrollTop;
+}
