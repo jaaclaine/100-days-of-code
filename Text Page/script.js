9b204096376eab66d657162cbd0150f14c2718de
Jaaclaine <jaquelinesodre@live.com>
2017-06-16 20:42:19 -0300

    I AM SO HAPPY WITH THIS

--- Text Page/script.js
+++ Text Page/script.js
@@ -11,3 +11,24 @@ function topBar(){
   }
   scrollBar = scrollTop;
 }
+
+// Mobile menu
+mobile.addEventListener("click", function(){
+  var mobile = document.getElementById("mobile");
+  var mobileIcon = document.getElementById("mobileIcon");
+  var menu = document.getElementById("top-menu");
+  menu.classList.toggle("open");
+  document.getElementsByTagName("body")[0].classList.toggle("block");
+  if(mobileIcon.classList.contains('fa-bars')){
+    mobileIcon.classList.remove("fa-bars");
+      mobileIcon.classList.add("fa-times");
+  } else{
+    mobileIcon.classList.add("fa-bars");
+      mobileIcon.classList.remove("fa-times");
+  }
+});
+window.addEventListener("resize", function(){
+  if(document.getElementsByTagName("body")[0].offsetWidth > 1000){
+    document.getElementsByTagName("body")[0].classList.remove("block");
+  }
+});
